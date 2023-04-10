d88fff2900c1b246fb49b7e450c9858f888cbd77
Prashant Jangid <prashantjangid@Prashants-MacBook-Pro.local>
2023-04-10 10:19:21 +0530

    Refactored code function

--- dpk.js
+++ dpk.js
@@ -3,11 +3,10 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate = event ? event : TRIVIAL_PARTITION_KEY;
+  let candidate = event ? JSON.stringify(event) : TRIVIAL_PARTITION_KEY;
 
   if (event) {
-    const data = JSON.stringify(event);
-    candidate = event.partitionKey ? event.partitionKey : crypto.createHash("sha3-512").update(data).digest("hex");
+    candidate = event.partitionKey ? event.partitionKey : crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
 
   if (candidate) {
